77846f73c4325cb96aa4a98f2868370e5cfb0f4f
gulizodakhan07 <abdullajonovagulzoda90@gmail.com>
2024-10-11 15:13:35 +0500

    fixed

---
+++ src/categories/category.service.spec.ts
@@ -0,0 +1,11 @@
+import { CategoryService } from "./category.service"
+
+describe("CategoryService",()=>{
+    service: CategoryService
+    const mockModel = {
+        findall: jest.fn(),
+        update: jest.fn(),
+        findByPk: jest.fn(),
+        
+    }
+})
